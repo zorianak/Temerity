fd31641cfe0a28e7e24707942740585268621c4a
Kim Holmes <yes@kimiscool.me>
2015-01-19 15:37:10 -0800

    Test handlebars :D

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router();
+
+// let's set up our config variable
+var config = {
+    title: "Temerity is cool",
+    author: {
+        name: 'rawr'
+    },
+    message: "I like cats.",
+    cool: "Kim is cool."
+};
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', config );
 });
 
 module.exports = router;

6e04fb17214eefb22021e9fec8bf1841f9a3bc75
Kim Holmes <yes@kimiscool.me>
2015-01-19 23:13:32 -0800

    Guild site now works with Keystone sorta

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,7 +1,37 @@
 var keystone = require('keystone');
 
-exports = module.exports = function(req, res) {
+var config = {
+    title: "Temerity is cool",
+    author: {
+        name: 'rawr'
+    },
+    message: "I like cats.",
+    cool: "Kim is cool.",
+    pageName: "Index",
+    adminNav: "../layouts/default/admin-nav.dust",
+    header: "../layouts/mainpage/header-main",
+    navTemplate: "../layouts/mainpage/nav-main",
+    content: "../layouts/mainpage/content-main",
+    footer: "../layouts/mainpage/footer-main",
+    css: "../layouts/mainpage/css-main",
+    scripts: [
+        {"src": "https://code.jquery.com/jquery-2.1.3.min.js"},
+        {"src": "/javascripts/randombg.js"},
+        {"src": "/javascripts/latest-tweet.js"},
+        {"src": "/javascripts/init-main.js"}
+    ],
+    navItems: [
+        {"name": "Home", "src": "/"},
+        {"name": "News", "src": "/news"},
+        {"name": "Forum", "src": "/forum"},
+        {"name": "The Guild", "src": "/guild"},
+        {"name": "Media", "src": "/media"},
+        {"name": "Recruitment", "src": "/recruitment"},
+        {"name": "Sales", "src": "/sales"}
+    ]
+};
 
+exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
 		locals = res.locals;
 
@@ -10,6 +40,6 @@ exports = module.exports = function(req, res) {
 	locals.section = 'home';
 
 	// Render the view
-	view.render('index');
+	view.render('index', config);
 
 };

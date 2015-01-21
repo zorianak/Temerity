9723b8a57f73097910e2c7c0546571f84c6b1538
Kim Holmes <yes@kimiscool.me>
2015-01-20 21:01:42 -0800

    Yassss working carousel thanks to Bootstrap

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,4 +1,5 @@
-var keystone = require('keystone');
+var keystone = require('keystone'),
+    Post = keystone.list('Post');
 
 var config = {
     title: "Temerity is cool",
@@ -16,6 +17,8 @@ var config = {
     css: "../layouts/mainpage/css-main",
     scripts: [
         {"src": "https://code.jquery.com/jquery-2.1.3.min.js"},
+        {"src": "/js/lib/bootstrap/bootstrap-3.2.0.min.js"},
+        {"src": "/js/lib/bootstrap/carousel.js"},
         {"src": "/javascripts/randombg.js"},
         {"src": "/javascripts/latest-tweet.js"},
         {"src": "/javascripts/init-main.js"}
@@ -28,8 +31,76 @@ var config = {
         {"name": "Media", "src": "/media"},
         {"name": "Recruitment", "src": "/recruitment"},
         {"name": "Sales", "src": "/sales"}
-    ]
+    ],
+    posts: []
 };
+
+var limit = 3;
+Post.model.find()
+    .limit(3)
+    .exec(function(err, posts) {
+        console.log(posts[0]);
+
+        // lazy coding 101, or I just want the stupid posts to work
+        if(posts.length === 1){
+            config.posts = [
+                {"post": posts[0].title,
+                    "slug": posts[0].slug,
+                    "title": posts[0].title,
+                     "date": posts[0].publishedDate,
+                     "contentBrief": posts[0].content.brief,
+                     "contentExtend": posts[0].content.extended,
+                     "postImg": posts[0].image
+                    }
+            ]
+        } else if(posts.length === 2){
+            config.posts = [
+                {"post": posts[0].title,
+                    "slug": posts[0].slug,
+                    "title": posts[0].title,
+                     "date": posts[0].publishedDate,
+                     "contentBrief": posts[0].content.brief,
+                     "contentExtend": posts[0].content.extended,
+                     "postImg": posts[0].image
+                    },
+                {"post": posts[1].title,
+                    "slug": posts[1].slug,
+                    "title": posts[1].title,
+                     "date": posts[1].publishedDate,
+                     "contentBrief": posts[1].content.brief,
+                     "contentExtend": posts[1].content.extended,
+                     "postImg": posts[1].image
+                    }
+            ]
+        } else {
+            config.posts = [
+                {"post": posts[0].title,
+                    "slug": posts[0].slug,
+                    "title": posts[0].title,
+                     "date": posts[0].publishedDate,
+                     "contentBrief": posts[0].content.brief,
+                     "contentExtend": posts[0].content.extended,
+                     "postImg": posts[0].image
+                    },
+                {"post": posts[1].title,
+                    "slug": posts[1].slug,
+                    "title": posts[1].title,
+                     "date": posts[1].publishedDate,
+                     "contentBrief": posts[1].content.brief,
+                     "contentExtend": posts[1].content.extended,
+                     "postImg": posts[1].image
+                    },
+                {"post": posts[2].title,
+                    "slug": posts[2].slug,
+                    "title": posts[2].title,
+                     "date": posts[2].publishedDate,
+                     "contentBrief": posts[2].content.brief,
+                     "contentExtend": posts[2].content.extended,
+                     "postImg": posts[2].image
+                    }
+            ]
+        }
+    });
 
 exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res),
@@ -38,6 +109,9 @@ exports = module.exports = function(req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+
+    // I want to get the first 3 blog posts and display those
+    console.log(config.posts);
 
 	// Render the view
 	view.render('index', config);
